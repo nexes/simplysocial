fb3fb5bde48c632a56595437f4823cb39e18958e
Joe Berria <joeberria@gmail.com>
2017-07-03 13:25:09 -0600

    added GET request for the users post count

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import url
 app_name = 'user'
 
 urlpatterns = [
-    url(r'^count/(?P<count_type>[a-zA-Z]+)/$', UserCounts.as_view(), name='count'),
+    url(r'^count/(?P<user_id>[0-9]+)/(?P<count_type>[a-zA-Z]+)/$', UserCounts.as_view(), name='count'),
     url(r'^description/$', UserDescription.as_view(), name='description'),
     url(r'^follow/new/$', UserFollowers.as_view(), name='newfollower'),
     url(r'^follow/remove/$', UserFollowers.as_view(), name='removefollower'),
--- user/views.py
+++ user/views.py
@@ -1,14 +1,42 @@
 """ handling view requests for user data """
-from django.http import HttpRequest, HttpResponse
+from user.models import Users
+import json
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpRequest, JsonResponse
 from django.views import View
 
-from user.models import Users
 
 
 class UserCounts(View):
     """ request the count of posts, followers... e.g /count/posts """
-    def get(self, request: HttpRequest):
-        pass
+    def get(self, request: HttpRequest, user_id: int, count_type: str):
+        resp = JsonResponse({})
+
+        try:
+            user = Users.objects.get(user_id__exact=user_id)
+        except ObjectDoesNotExist:
+            resp.status_code = 400
+            resp.content = json.dumps({
+                'message': 'bad user id {}, user not found'.format(user_id)
+            })
+            return resp
+
+        if count_type.lower() == 'posts':
+            count = user.posts_set.count()
+            resp.status_code = 200
+            resp.content = json.dumps({
+                'count': count
+            })
+        elif count_type.lower() == 'followers':
+            # TODO - this needs to be done when we create followers model
+            resp.status_code = 200
+            resp.content = json.dumps({
+                'followers': 3
+            })
+
+        return resp
+
 
 class UserDescription(View):
     """ GET: returns the users personal description
@@ -19,6 +47,7 @@ class UserDescription(View):
 
     def post(self, request: HttpRequest):
         pass
+
 
 class UserFollowers(View):
     """ GET: can check if the user is following another user
