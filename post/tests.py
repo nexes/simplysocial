fe64057fc0b35ecc2d46064bbdd655de25bd43b3
Joe Berria <joeberria@gmail.com>
2017-07-07 14:10:34 -0600

    posts can be deleted, updated tests

--- post/tests.py
+++ post/tests.py
@@ -1,5 +1,5 @@
 from user.models import Users
-from post.models import Posts
+from base64 import b64encode
 import json
 
 from django.utils import timezone
@@ -10,25 +10,9 @@ from django.core.signing import Signer
 
 @tag('userpost')
 class UserPostCreate(TestCase):
-    def _create_user(self, username: str, password: str):
-        salt = 'blahfffffj349feiblah123'
-        signer = Signer(salt=salt)
-
-        user = Users()
-        user.user_id = 324
-        user.first_name = 'Billy'
-        user.last_name = 'Bobtest'
-        user.user_name = username
-        user.last_login_date = timezone.now()
-        user.password_hash = signer.signature(password)
-        user.salt_hash = salt
-        user.save()
-
-        return user
-
-    def _login_user(self, user: Users, password: str):
+    def _login_user(self, password: str):
         url = '/snaplife/api/auth/user/login/'
-        data = json.dumps({'username': user.user_name, 'password': password})
+        data = json.dumps({'username': self.user.user_name, 'password': password})
         resp = self.client.post(url, data, content_type='application/json')
 
         print('\tuser post: login status {}'.format(resp.status_code == 200))
@@ -37,18 +21,36 @@ class UserPostCreate(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        salt = 'blahfffffj349feiblah123'
+        signer = Signer(salt=salt)
+
+        user = Users()
+        user.user_id = 324
+        user.first_name = 'Billy'
+        user.last_name = 'Bobtest'
+        user.user_name = 'myUsername'
+        user.last_login_date = timezone.now()
+        user.password_hash = signer.signature('password123')
+        user.salt_hash = salt
+        user.save()
+
+        cls.user = user
         cls.client = Client()
 
     def test_post_create(self):
-        user = self._create_user('joe', 'password123')
-        self._login_user(user, 'password123')
+        self._login_user('password123')
 
         url = '/snaplife/api/user/posts/create/'
-        url2 = '/snaplife/api/user/count/{}/posts/'.format(user.user_id)
+        url2 = '/snaplife/api/user/count/{}/posts/'.format(self.user.user_id)
+
+        with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
+            encode = b64encode(f.read())
+
         data = json.dumps({
-            'image': 'not testable from here',
-            'message': 'This will be apart of the the image post',
-            'userid': user.user_id
+            'image': encode.decode('utf-8'),
+            'message': 'this is a description of our post',
+            'title': 'a test title!!!',
+            'userid': self.user.user_id
         })
 
         resp = self.client.post(url, data, content_type='application/json')
@@ -62,14 +64,12 @@ class UserPostCreate(TestCase):
         self.assertContains(resp2, 'count')
 
     def test_post_without_signin(self):
-        user = self._create_user('joe', 'password123')
-
         url = '/snaplife/api/user/posts/create/'
-        url2 = '/snaplife/api/user/count/{}/posts/'.format(user.user_id)
+        url2 = '/snaplife/api/user/count/{}/posts/'.format(self.user.user_id)
         data = json.dumps({
             'image': 'not testable from here',
             'message': 'This will be apart of the the image post',
-            'userid': user.user_id
+            'userid': self.user.user_id
         })
 
         resp = self.client.post(url, data, content_type='application/json')
@@ -81,3 +81,30 @@ class UserPostCreate(TestCase):
         print('\tpost_create: checking post count for user not signed in: count = {}'.format(resp2.json()['count']))
         self.assertEqual(resp2.status_code, 200)
         self.assertContains(resp2, 'count')
+
+    def test_post_delete(self):
+        url_delete = '/snaplife/api/user/posts/delete/'
+        url_create = '/snaplife/api/user/posts/create/'
+        self._login_user('password123')
+
+        with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
+            encode = b64encode(f.read())
+
+        create_data = json.dumps({
+            'image': encode.decode('utf-8'),
+            'message': 'this is a description of our post, a test post message',
+            'title': 'a test title!!!',
+            'userid': self.user.user_id
+        })
+        resp = self.client.post(url_create, create_data, content_type='application/json')
+        print('\tpost_delete: creating new post signing in: {}'.format(resp.status_code))
+
+        delete_data = json.dumps({
+            'userid': self.user.user_id,
+            'title': 'a test title!!!'
+        })
+        resp2 = self.client.post(url_delete, delete_data, content_type='application/json')
+        print('\tpost_delete: deleting new post: {}, {}'.format(resp2.status_code, resp2.json()['message']))
+
+        self.assertEqual(resp2.status_code, 200)
+        self.assertContains(resp2, 'success')
--- post/views.py
+++ post/views.py
@@ -1,4 +1,5 @@
 from random import random
+from lifesnap.aws import AWS
 import json
 
 from user.models import Users
@@ -12,12 +13,14 @@ from django.core.exceptions import ObjectDoesNotExist
 class PostCreate(View):
     """ a signed in user can create a new post
         POST: required json object {
-            'image': the photo to post,
+            'image': the photo to post, the front end will encode to base64 before sending.
             'message': optional - if you want a message with the photo,
+            'title': optional - if you want to title your post,
             'userid': the users user_id
         }
     """
     def post(self, request: HttpRequest):
+        s3_bucket = AWS('snap-life')
         resp = JsonResponse({})
         resp.status_code = 200
         new_post = Posts()
@@ -50,8 +53,14 @@ class PostCreate(View):
 
         #create new post and assign to the user
         new_post.post_id = int(random() * 1000000)
-        new_post.image_url = 'we need to setup s3 bucket url',
-        new_post.message = req_json['message']
+        image_name = '{}{}.png'.format(user.user_id, new_post.post_id)
+
+        url = s3_bucket.upload_image(image_name, req_json['image'])
+
+        new_post.message = req_json.get('message', '')
+        new_post.message_title = req_json.get('title', '')
+        new_post.image_name = image_name
+        new_post.image_url = url
         new_post.save()
         user.posts_set.add(new_post)
 
@@ -61,10 +70,81 @@ class PostCreate(View):
         return resp
 
 
+#TODO: refactor all of these resp.status_code resp.content stuff DRY DRY DRY
 class PostDelete(View):
-    """  """
+    """ Delete a post if found, postid and title are optional but one needs to be set
+        Post: required json object: {
+            userid: user id,
+            postid: <optional> can search by post id,
+            title: <optional> can search by post title,
+        }
+        if both postid and title is present, postid will be prefered.
+    """
     def post(self, request: HttpRequest):
-        pass
+        s3_bucket = AWS('snap-life')
+        resp = JsonResponse({})
+        resp.status_code = 200
+
+        try:
+            req_json = json.loads(request.body.decode('UTF-8'))
+        except json.JSONDecodeError:
+            resp.status_code = 400
+            resp.content = json.dumps({
+                "message": "request decode error, bad data sent to the server"
+                })
+            return resp
+
+        try:
+            user = Users.objects.get(user_id__exact=req_json.get('userid', ''))
+        except ObjectDoesNotExist:
+            resp.status_code = 400
+            resp.content = json.dumps({
+                "message": "userid {} is not found".format(req_json['userid'])
+                })
+            return resp
+
+        #check user is logged in via sessions
+        if request.session.get('{}'.format(user.user_id), False) is False:
+            resp.status_code = 400
+            resp.content = json.dumps({
+                'message': 'user is not signed in'
+            })
+            return resp
+
+        if req_json.get('postid') is None and req_json.get('title') is None:
+            resp.status_code = 400
+            resp.content = json.dumps({
+                'message': 'postid or title must be present'
+            })
+            return resp
+
+        if req_json.get('postid') is not None:
+            try:
+                post = user.posts_set.get(post_id__exact=int(req_json['postid']))
+            except ObjectDoesNotExist:
+                resp.status_code = 400
+                resp.content = json.dumps({
+                    'message': 'postid {} is not found'.format(req_json['postid'])
+                })
+                return resp
+        elif req_json.get('title') is not None:
+            try:
+                post = user.posts_set.get(message_title__exact=req_json['title'])
+            except ObjectDoesNotExist:
+                resp.status_code = 400
+                resp.content = json.dumps({
+                    'message': 'post with title {} is not found'.format(req_json['title'])
+                })
+                return resp
+
+        s3_bucket.remove_image(key_name=post.image_name)
+        post.delete()
+
+        resp.content = json.dumps({
+            'message': 'success',
+            'postcount': '{}'.format(user.posts_set.count())
+        })
+        return resp
 
 
 class PostUpdate(View):
