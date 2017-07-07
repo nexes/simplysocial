d1b6af6865c354f89a81992558b0d18bff67535f
Joe Berria <joeberria@gmail.com>
2017-07-07 10:49:19 -0600

    adding image_name field to post model

--- post/models.py
+++ post/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Posts(models.Model):
     post_id = models.IntegerField(blank=False, unique=True)
     image_url = models.CharField(blank=False, unique=True, max_length=100)
+    image_name = models.CharField(blank=False, unique=True, max_length=50)
     message = models.CharField(blank=True, max_length=254)
     creation_date = models.DateTimeField(auto_now_add=True)
     view_count = models.IntegerField(default=0)
