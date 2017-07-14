ed72511377272dd1c8124ffb953e9dd254e3f9c7
Joe Berria <joeberria@gmail.com>
2017-07-14 11:46:07 -0700

    removed comments on post tests

--- post/tests.py
+++ post/tests.py
@@ -39,119 +39,119 @@ class UserPostCreate(TestCase):
         cls.user = user
         cls.client = Client()
 
-    # def test_post_create(self):
-    #     self._login_user('password123')
+    def test_post_create(self):
+        self._login_user('password123')
 
-    #     url = '/snaplife/api/user/posts/create/'
-    #     url2 = '/snaplife/api/user/count/{}/posts/'.format(self.user.user_id)
+        url = '/snaplife/api/user/posts/create/'
+        url2 = '/snaplife/api/user/count/{}/posts/'.format(self.user.user_id)
 
-    #     with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
-    #         encode = b64encode(f.read())
+        with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
+            encode = b64encode(f.read())
 
-    #     data = json.dumps({
-    #         'image': encode.decode('utf-8'),
-    #         'message': 'this is a description of our post',
-    #         'title': 'a test title!!!',
-    #         'userid': self.user.user_id
-    #     })
+        data = json.dumps({
+            'image': encode.decode('utf-8'),
+            'message': 'this is a description of our post',
+            'title': 'a test title!!!',
+            'userid': self.user.user_id
+        })
 
-    #     resp = self.client.post(url, data, content_type='application/json')
-    #     print('\tpost_create: creating new post for user: {}, postid {}'.format(resp.status_code, resp.json()['postid']))
-    #     self.assertEqual(resp.status_code, 200)
-    #     self.assertContains(resp, 'success')
+        resp = self.client.post(url, data, content_type='application/json')
+        print('\tpost_create: creating new post for user: {}, postid {}'.format(resp.status_code, resp.json()['postid']))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'success')
 
-    #     resp2 = self.client.get(url2)
-    #     print('\tpost_create: checking post count for user: count = {}'.format(resp2.json()['count']))
-    #     self.assertEqual(resp2.status_code, 200)
-    #     self.assertContains(resp2, 'count')
+        resp2 = self.client.get(url2)
+        print('\tpost_create: checking post count for user: count = {}'.format(resp2.json()['count']))
+        self.assertEqual(resp2.status_code, 200)
+        self.assertContains(resp2, 'count')
 
-    # def test_post_without_signin(self):
-    #     url = '/snaplife/api/user/posts/create/'
-    #     url2 = '/snaplife/api/user/count/{}/posts/'.format(self.user.user_id)
-    #     data = json.dumps({
-    #         'image': 'not testable from here',
-    #         'message': 'This will be apart of the the image post',
-    #         'userid': self.user.user_id
-    #     })
+    def test_post_without_signin(self):
+        url = '/snaplife/api/user/posts/create/'
+        url2 = '/snaplife/api/user/count/{}/posts/'.format(self.user.user_id)
+        data = json.dumps({
+            'image': 'not testable from here',
+            'message': 'This will be apart of the the image post',
+            'userid': self.user.user_id
+        })
 
-    #     resp = self.client.post(url, data, content_type='application/json')
-    #     print('\tpost_create: creating new post without signing in: {}'.format(resp.status_code))
-    #     self.assertEqual(resp.status_code, 400)
-    #     self.assertContains(resp, 'user is not signed in', status_code=400)
+        resp = self.client.post(url, data, content_type='application/json')
+        print('\tpost_create: creating new post without signing in: {}'.format(resp.status_code))
+        self.assertEqual(resp.status_code, 400)
+        self.assertContains(resp, 'user is not signed in', status_code=400)
 
-    #     resp2 = self.client.get(url2)
-    #     print('\tpost_create: checking post count for user not signed in: count = {}'.format(resp2.json()['count']))
-    #     self.assertEqual(resp2.status_code, 200)
-    #     self.assertContains(resp2, 'count')
+        resp2 = self.client.get(url2)
+        print('\tpost_create: checking post count for user not signed in: count = {}'.format(resp2.json()['count']))
+        self.assertEqual(resp2.status_code, 200)
+        self.assertContains(resp2, 'count')
 
-    # def test_post_delete(self):
-    #     url_delete = '/snaplife/api/user/posts/delete/'
-    #     url_create = '/snaplife/api/user/posts/create/'
-    #     self._login_user('password123')
+    def test_post_delete(self):
+        url_delete = '/snaplife/api/user/posts/delete/'
+        url_create = '/snaplife/api/user/posts/create/'
+        self._login_user('password123')
 
-    #     with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
-    #         encode = b64encode(f.read())
+        with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
+            encode = b64encode(f.read())
 
-    #     create_data = json.dumps({
-    #         'image': encode.decode('utf-8'),
-    #         'message': 'this is a description of our post, a test post message',
-    #         'title': 'a test title!!!',
-    #         'userid': self.user.user_id
-    #     })
-    #     resp = self.client.post(url_create, create_data, content_type='application/json')
-    #     print('\tpost_delete: creating new post signing in: {}'.format(resp.status_code))
+        create_data = json.dumps({
+            'image': encode.decode('utf-8'),
+            'message': 'this is a description of our post, a test post message',
+            'title': 'a test title!!!',
+            'userid': self.user.user_id
+        })
+        resp = self.client.post(url_create, create_data, content_type='application/json')
+        print('\tpost_delete: creating new post signing in: {}'.format(resp.status_code))
 
-    #     delete_data = json.dumps({
-    #         'userid': self.user.user_id,
-    #         'title': 'a test title!!!'
-    #     })
-    #     resp2 = self.client.post(url_delete, delete_data, content_type='application/json')
-    #     print('\tpost_delete: deleting new post: {}, {}'.format(resp2.status_code, resp2.json()['message']))
+        delete_data = json.dumps({
+            'userid': self.user.user_id,
+            'title': 'a test title!!!'
+        })
+        resp2 = self.client.post(url_delete, delete_data, content_type='application/json')
+        print('\tpost_delete: deleting new post: {}, {}'.format(resp2.status_code, resp2.json()['message']))
 
-    #     self.assertEqual(resp2.status_code, 200)
-    #     self.assertContains(resp2, 'success')
+        self.assertEqual(resp2.status_code, 200)
+        self.assertContains(resp2, 'success')
 
-    # def test_post_search(self):
-    #     self._login_user('password123')
-    #     q_str = quote('test title')
-    #     now = datetime(2017, 7, 1)
+    def test_post_search(self):
+        self._login_user('password123')
+        q_str = quote('test title')
+        now = datetime(2017, 7, 1)
 
-    #     url = '/snaplife/api/user/posts/create/'
-    #     title_search = '/snaplife/api/user/posts/search/title/{}/{}/3/'.format(self.user.user_id, q_str)
-    #     date_search = '/snaplife/api/user/posts/search/range/{}/{}/3/'.format(self.user.user_id, int(now.timestamp()))
+        url = '/snaplife/api/user/posts/create/'
+        title_search = '/snaplife/api/user/posts/search/title/{}/{}/3/'.format(self.user.user_id, q_str)
+        date_search = '/snaplife/api/user/posts/search/range/{}/{}/3/'.format(self.user.user_id, int(now.timestamp()))
 
-    #     with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
-    #         encode = b64encode(f.read())
+        with open('/Users/jberria/Pictures/test1sprites0.png', 'rb') as f:
+            encode = b64encode(f.read())
 
-    #     data = json.dumps({
-    #         'image': encode.decode('utf-8'),
-    #         'message': 'this is a description of our post',
-    #         'title': 'a test title!!!',
-    #         'userid': self.user.user_id
-    #     })
-    #     data1 = json.dumps({
-    #         'image': encode.decode('utf-8'),
-    #         'message': 'this is another post, the second one',
-    #         'title': 'test title!!!',
-    #         'userid': self.user.user_id
-    #     })
-    #     data2 = json.dumps({
-    #         'image': encode.decode('utf-8'),
-    #         'message': 'django python post about this, i dont know',
-    #         'title': 'a test',
-    #         'userid': self.user.user_id
-    #     })
-    #     self.client.post(url, data, content_type='application/json')
-    #     self.client.post(url, data1, content_type='application/json')
-    #     self.client.post(url, data2, content_type='application/json')
+        data = json.dumps({
+            'image': encode.decode('utf-8'),
+            'message': 'this is a description of our post',
+            'title': 'a test title!!!',
+            'userid': self.user.user_id
+        })
+        data1 = json.dumps({
+            'image': encode.decode('utf-8'),
+            'message': 'this is another post, the second one',
+            'title': 'test title!!!',
+            'userid': self.user.user_id
+        })
+        data2 = json.dumps({
+            'image': encode.decode('utf-8'),
+            'message': 'django python post about this, i dont know',
+            'title': 'a test',
+            'userid': self.user.user_id
+        })
+        self.client.post(url, data, content_type='application/json')
+        self.client.post(url, data1, content_type='application/json')
+        self.client.post(url, data2, content_type='application/json')
 
-    #     resp = self.client.get(title_search)
-    #     self.assertEqual(resp.status_code, 200)
-    #     self.assertContains(resp, 'success')
+        resp = self.client.get(title_search)
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'success')
 
-    #     resp = self.client.get(date_search)
-    #     self.assertEqual(resp.status_code, 200)
-    #     self.assertContains(resp, 'success')
+        resp = self.client.get(date_search)
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'success')
 
     def test_post_update(self):
         self._login_user('password123')
