ddcd3bd0ae8c9d7fbf43333169c532de758bc55f
Joe Berria <joeberria@gmail.com>
2017-07-17 12:06:10 -0700

    update image name length

--- post/models.py
+++ post/models.py
@@ -9,7 +9,7 @@ class Posts(models.Model):
 
     post_id = models.IntegerField(blank=False, unique=True)
     image_url = models.CharField(blank=False, unique=True, max_length=100)
-    image_name = models.CharField(blank=False, unique=True, max_length=50)
+    image_name = models.CharField(blank=False, unique=True, max_length=100)
     message = models.CharField(blank=True, max_length=254)
     message_title = models.CharField(blank=True, max_length=100)
     creation_date = models.DateTimeField(auto_now_add=True)
