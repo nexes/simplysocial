0e5a6aef016d954a591ea9e333b648b9e683fe57
Joe Berria <joeberria@gmail.com>
2017-07-20 11:35:11 -0700

    adding comment app

---
+++ comment/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from post.models import Posts
+
+
+class Comments(models.Model):
+    class Meta:
+        ordering = ['-creation_date']
+
+    comment_id = models.IntegerField(unique=True, blank=False)
+    author_id = models.IntegerField(blank=False)
+    creation_date = models.DateField(auto_now_add=True)
+    message = models.CharField(max_length=255, blank=False)
+    like_count = models.IntegerField(default=0)
+    report_count = models.IntegerField(default=0)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, null=True)
