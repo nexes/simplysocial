290875e5392967deb66805e698a2b315fcdeb98c
Joe Berria <joeberria@gmail.com>
2017-09-11 15:54:01 -0700

    adding post author data, making it easier to show posts from followers

--- post/models.py
+++ post/models.py
@@ -10,6 +10,8 @@ class Posts(models.Model):
     post_id = models.IntegerField(blank=False, unique=True)
     image_url = models.CharField(blank=True, max_length=100)
     image_name = models.CharField(blank=True, max_length=100)
+    author_username = models.CharField(blank=False, max_length=40)
+    author_profile_url = models.CharField(max_length=100, blank=True)
     message = models.CharField(max_length=254)
     message_title = models.CharField(blank=True, max_length=100)
     creation_date = models.DateTimeField(auto_now_add=True)
